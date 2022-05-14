c0a9f61e8d58642b933af753dc6b6a2bae08ef27
tluyen2602@gmail.com <tluyen2602@gmail.com>
2022-05-14 22:16:17 +0700

    tao database

---
+++ DataBase/DATABASE.sql
@@ -0,0 +1,63 @@
+create database QUANLITHIETBICONGNGHE_KLN
+use QUANLITHIETBICONGNGHE_KLN
+GO 
+create table KhachHang
+(
+	maKH int  primary key not null,
+	tenKH nvarchar(50) not null,
+	diaChi nvarchar(80) not null,
+	SDT varchar(11) not null,
+	gioiTinh bit null
+)
+go 
+create table NhanVien
+(
+	maNV int not null primary key,
+	tenNV nvarchar(50) not null,
+	vaiTro nvarchar(50) not null,
+	diaChi nvarchar(80) ,
+	SDT varchar(11) not null,
+	ngaySinh date ,
+	gioiTinh bit ,
+	email varchar(50),
+	trangThai nvarchar(30)
+)
+go 
+ create table SanPham
+ (
+	maSP int not null primary key,
+	tenSP nvarchar(50) not null,
+	donGiaBan money not null,
+	soLuongHienCon int not null
+
+ )
+ go 
+ create table HoaDon
+ (
+	maDH int not null primary key,
+	tenSP nvarchar(50) not null,
+	soLuongDat int not null,
+	thoiGianBaoHanh date ,
+	diaChiGiaoHang nvarchar(80) not null,
+	phuongThucThanhToan nvarchar(30) not null,
+	thoiGianGiaoHang date,
+	thanhTien money,
+	maKH int  not null,
+	maNV int not null,
+	maSP int not null,
+	constraint FK_HDKH_KH foreign key (maKH) references KhachHang(maKH),
+	constraint FK_HDNV_NV foreign key (maNV) references NhanVien(maNV),
+	constraint FK_HDSP_SP foreign key (maSP) references SanPham(maSP)
+)
+go 
+create table KhuyenMai
+(
+	maKM int not null primary key,
+	tenCTKM nvarchar(50) not null,
+	mucGiamGia int not null,
+	ngayBatDau date ,
+	ngayKetThuc date,
+	moTa nvarchar(50),
+	maSP int not null,
+	constraint FK_KMSP_SP foreign key (maSP) references SanPham(maSP)
+)

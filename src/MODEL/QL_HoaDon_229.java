c3f7c9c98bcfc6211dbaa134fcbd878b1f41d88c
tluyen2602@gmail.com <tluyen2602@gmail.com>
2022-05-18 01:51:30 +0700

    tao model QL_HoaDon_229

---
+++ src/MODEL/QL_HoaDon_229.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODEL;
+
+/**
+ *
+ * @author Admin
+ */
+public class QL_HoaDon_229 {
+
+    int maHD;
+    String tenSP;
+    private int soLuongDat;
+    private String thoiGianBaoHanh;
+    private String diaChiGiaoHang;
+    private String phuongThucThanhToan;
+    private String thoiGianGiaoHang;
+    private Double thanhTien;
+
+    public QL_HoaDon_229() {
+    }
+
+    public int getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(int maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public int getSoLuongDat() {
+        return soLuongDat;
+    }
+
+    public void setSoLuongDat(int soLuongDat) {
+        this.soLuongDat = soLuongDat;
+    }
+
+    public String getThoiGianBaoHanh() {
+        return thoiGianBaoHanh;
+    }
+
+    public void setThoiGianBaoHanh(String thoiGianBaoHanh) {
+        this.thoiGianBaoHanh = thoiGianBaoHanh;
+    }
+
+    public String getDiaChiGiaoHang() {
+        return diaChiGiaoHang;
+    }
+
+    public void setDiaChiGiaoHang(String diaChiGiaoHang) {
+        this.diaChiGiaoHang = diaChiGiaoHang;
+    }
+
+    public String getPhuongThucThanhToan() {
+        return phuongThucThanhToan;
+    }
+
+    public void setPhuongThucThanhToan(String phuongThucThanhToan) {
+        this.phuongThucThanhToan = phuongThucThanhToan;
+    }
+
+    public String getThoiGianGiaoHang() {
+        return thoiGianGiaoHang;
+    }
+
+    public void setThoiGianGiaoHang(String thoiGianGiaoHang) {
+        this.thoiGianGiaoHang = thoiGianGiaoHang;
+    }
+
+    public Double getThanhTien() {
+        return thanhTien;
+    }
+
+    public void setThanhTien(Double thanhTien) {
+        this.thanhTien = thanhTien;
+    }
+    
+
+}

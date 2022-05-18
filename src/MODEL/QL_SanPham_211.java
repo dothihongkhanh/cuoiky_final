2873343f8fdf7945276fa7668bb067b624040fbb
VanKhanh <khanhle0767251658@gmail.com>
2022-05-18 08:55:24 +0700

    tao Model QL_SsanPham

---
+++ src/MODEL/QL_SanPham_211.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODEL;
+
+/**
+ *
+ * @author Khanhle
+ */
+public class QL_SanPham_211 {
+     private int maHH_211;
+    private String tenHH_211;
+    private String gia_211;
+    private String donvitinh_211;
+    private String soLuongHienCon_211;
+
+ public int getMaHH_211() {
+        return maHH_211;
+    }
+
+    public void setMaHH_211(int maHH_211) {
+        this.maHH_211 = maHH_211;
+    }
+
+    public String getTenHH_211() {
+        return tenHH_211;
+    }
+
+    public void setTenHH_211(String tenHH_211) {
+        this.tenHH_211 = tenHH_211;
+    }
+
+    public String getGia_211() {
+        return gia_211;
+    }
+
+    public void setGia_211(String Gia_211) {
+        this.gia_211 = gia_211;
+    }
+
+    public String getDơnvitinh_211() {
+        return donvitinh_211;
+    }
+
+    public void setDonvitinh_211(String Donvitinh_211) {
+        this.donvitinh_211 = donvitinh_211;
+    }
+
+    public String getSoLuongHienCon_211(String soLuongHienCon_211) {
+        return soLuongHienCon_211;
+    }
+
+    public void setSoLuongHienCon_211(String soLuongHienCon_211) {
+        this.soLuongHienCon_211 = soLuongHienCon_211;
+    }
+}

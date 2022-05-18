a2afd3f341e3edd642f08fcc742ec4f21fbf5a2b
teyugn901 <thaonguyet02vn@gmail.com>
2022-05-18 09:12:13 +0700

    model qlnv_239

---
+++ src/MODEL/QL_NhanVien_239.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODEL;
+
+/**
+ *
+ * @author DELL
+ */
+public class QL_NhanVien_239 {
+    private int maNV_239 ;
+    private String tenNV_239;
+    private String vaiTro_239;
+    private String diaChi_239;
+    private String dienthoai_239;
+    private String ngaySinh_239;
+    private String gioiTinh_239;
+    private String email_239;
+    private String trangThai_239;
+
+    public int getMaNV_239() {
+        return maNV_239;
+    }
+
+    public void setMaNV_239(int maNV_239) {
+        this.maNV_239 = maNV_239;
+    }
+
+    public String getTenNV_239() {
+        return tenNV_239;
+    }
+
+    public void setTenNV_239(String tenNV_239) {
+        this.tenNV_239 = tenNV_239;
+    }
+
+    public String getVaiTro_239() {
+        return vaiTro_239;
+    }
+
+    public void setVaiTro_239(String vaiTro_239) {
+        this.vaiTro_239 = vaiTro_239;
+    }
+
+    public String getDiaChi_239() {
+        return diaChi_239;
+    }
+
+    public void setDiaChi_239(String diaChi_239) {
+        this.diaChi_239 = diaChi_239;
+    }
+
+    public String getDienthoai_239() {
+        return dienthoai_239;
+    }
+
+    public void setDienthoai_239(String dienthoai_239) {
+        this.dienthoai_239 = dienthoai_239;
+    }
+
+    public String getNgaySinh_239() {
+        return ngaySinh_239;
+    }
+
+    public void setNgaySinh_239(String ngaySinh_239) {
+        this.ngaySinh_239 = ngaySinh_239;
+    }
+
+    public String getGioiTinh_239() {
+        return gioiTinh_239;
+    }
+
+    public void setGioiTinh_239(String gioiTinh_239) {
+        this.gioiTinh_239 = gioiTinh_239;
+    }
+
+    public String getEmail_239() {
+        return email_239;
+    }
+
+    public void setEmail_239(String email_239) {
+        this.email_239 = email_239;
+    }
+
+    public String getTrangThai_239() {
+        return trangThai_239;
+    }
+
+    public void setTrangThai_239(String trangThai_239) {
+        this.trangThai_239 = trangThai_239;
+    }
+}

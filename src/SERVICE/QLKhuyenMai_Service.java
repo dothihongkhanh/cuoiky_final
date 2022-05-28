ff2dd2f426540037876ae08ad87a5457f975def8
dothihongkhanh <dothihongkhanh22@gmail.com>
2022-05-28 18:00:02 +0700

    add QLKhuyenMai_Service

---
+++ src/SERVICE/QLKhuyenMai_Service.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SERVICE;
+
+import DAO.QLKhuyenMai_Dao;
+import MODEL.QL_KhuyenMai_212;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author DELL
+ */
+public class QLKhuyenMai_Service {
+     private QLKhuyenMai_Dao kmDao;
+
+    public QLKhuyenMai_Service() {
+        kmDao = new QLKhuyenMai_Dao();
+    }
+
+    public List<QL_KhuyenMai_212> getAllUsers() throws SQLException {
+        return kmDao.getAllUsers();
+    }
+
+    public void addKM(QL_KhuyenMai_212 km) throws SQLException {
+        kmDao.addKM(km);
+    }
+
+}

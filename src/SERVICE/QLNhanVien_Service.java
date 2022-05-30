cd3129043eb9f11be05eb2a80fa4ac03e1f5280c
teyugn901 <thaonguyet02vn@gmail.com>
2022-05-30 13:58:47 +0700

    Qlnv Service

---
+++ src/SERVICE/QLNhanVien_Service.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SERVICE;
+
+/**
+ *
+ * @author DELL
+ */
+import DAO.QLNhanVien_Dao;
+import MODEL.QL_NhanVien_239;
+import java.sql.SQLException;
+import java.util.List;
+public class QLNhanVien_Service {
+    private final QLNhanVien_Dao nvDao;
+
+    public QLNhanVien_Service() {
+        nvDao = new QLNhanVien_Dao();
+    }
+
+    public List<QL_NhanVien_239> getAllUsers() throws SQLException {
+        return nvDao.getAllUsers();
+    }
+
+    public void addPhong(QL_NhanVien_239 nv) throws SQLException {
+        nvDao.addNV(nv);
+    }
+
+    public void deletePhong(int id) throws SQLException {
+        nvDao.deleteNV(id);
+    }
+}
